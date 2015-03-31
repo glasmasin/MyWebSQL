fab3037bb257f2c3d357b9ee411691c3483593b2
Tom Horwood <tomhorwood@gmail.com>
2015-03-31 17:45:41 +1000

    Fix to ensure only functions have incrementing postfix

--- modules/views/pgsql/objtree.php
+++ modules/views/pgsql/objtree.php
@@ -4,117 +4,155 @@
  */
  class NodeGrouping
  {
- 	public $rule = null;
- 	private $subGroups = array();
+    public $rule = null;
+    private $subGroups = array();
 
- 	function __construct($rule)
- 	{
- 		$this->rule = $rule;
- 	}
- 	function getGroupOutput($nodes) {
- 		$twigs = array();
- 		foreach ($nodes as $node) {
- 			$group = $this->getSubGroup($node);
- 			$this->putInGroup($group, $node);
- 		}
- 		foreach ($this->subGroups as $subGroup => $children) {
- 			if (count($children) === 1 or $subGroup === "noGroup") {
- 				foreach ($children as $child) {
- 					$twigs[] = $child;
- 				}
- 			} else {
- 				$twigs[] = array("text" => $subGroup . ' [' . count($children) . ']', "children" => $children);
- 			}
- 		}
- 		return $twigs;
- 	}
+    /**
+     * Initialise the grouping with a rule to group by
+     * @param [string] $rule Regex to group by
+     */
+    function __construct($rule)
+    {
+        $this->rule = $rule;
+    }
 
- 	function putInGroup($groupName, $node) {
- 		if(!array_key_exists($groupName, $this->subGroups)) {
- 			$this->subGroups[$groupName] = array();
- 		}
- 		$this->subGroups[$groupName][] = $node;
- 	}
+    function getGroupOutput($nodes) {
+        $twigs = array();
+        foreach ($nodes as $node) {
+            $group = $this->getSubGroup($node);
+            $this->putInGroup($group, $node);
+        }
+        foreach ($this->subGroups as $subGroup => $children) {
+            if (count($children) === 1 or $subGroup === "noGroup") {
+                foreach ($children as $child) {
+                    $twigs[] = $child;
+                }
+            } else {
+                $twigs[] = array("text" => $subGroup . ' [' . count($children) . ']', "children" => $children);
+            }
+        }
+        return $twigs;
+    }
 
- 	function getSubGroup($node) {
- 		$matches = null;
- 		if (preg_match($this->rule, $node['text'], $matches)) {
- 			return $matches[1];
- 		}
- 		return "noGroup";
- 	}
+    function putInGroup($groupName, $node) {
+        if(!array_key_exists($groupName, $this->subGroups)) {
+            $this->subGroups[$groupName] = array();
+        }
+        $this->subGroups[$groupName][] = $node;
+    }
+
+    function getSubGroup($node) {
+        $matches = null;
+        if (preg_match($this->rule, $node['text'], $matches)) {
+            return $matches[1];
+        }
+        return "noGroup";
+    }
  }
 
-$objTypes = array(
-	(object) array('name' => 'table', 'class' => 'tablef', 'groupby' => '/^tbl(.+?)_/'),
-	(object) array('name' => 'view', 'class' => 'viewf', 'groupby' => '/^v_(.+?)(_|$)/'),
-	(object) array('name' => 'procedure', 'class' => 'procf', 'groupby' => '/^p_(.+?)_/'),
-	(object) array('name' => 'function', 'class' => 'funcf', 'groupby' => '/^(f_.+?|_sys|[a-z]{3,})[^a-z]/'),
-	(object) array('name' => 'trigger', 'class' => 'trigf', 'groupby' => '/(.+?)_/'),
-	(object) array('name' => 'event', 'class' => 'evtf', 'groupby' => false),
-	);
-$groupby_rule = "/^.{6}/";
+/**
+*
+*/
+class ObjTree
+{
 
-//print '<ul id="tablelist" class="filetree">';
-$trunks = array();
-foreach($data['schemas'] as $schema) {
+    private $objectTypes = array(
+        array('name' => 'table', 'class' => 'tablef', 'groupby' => '/^tbl(.+?)_/'),
+        array('name' => 'view', 'class' => 'viewf', 'groupby' => '/^v_(.+?)(_|$)/'),
+        array('name' => 'procedure', 'class' => 'procf', 'groupby' => '/^p_(.+?)_/'),
+        array('name' => 'function', 'class' => 'funcf', 'groupby' => '/^(f_.+?|_sys|[a-z]{3,})[^a-z]/'),
+        array('name' => 'trigger', 'class' => 'trigf', 'groupby' => '/(.+?)_/'),
+        array('name' => 'event', 'class' => 'evtf', 'groupby' => false),
+        );
+    private $groupby_rule = "/^.{6}/";
+    private $tree = array();
+    private $data = array();
+    public $messages = array();
 
-	$schema_id = 's_'.Html::id($schema);
-	//print '<li id="'.$schema_id.'"><span class="schmf">'.htmlspecialchars($schema).'</span>';
-	//print '<ul class="filetree">';
-	$branches = array();
-	foreach($objTypes as $objType) {
-		$objPural = $objType->name . 's';
-		if (isset($data[$objPural])) {
-			$objects = isset($data[$objPural]) ? $data[$objPural][$schema] : array();
-			//print '<span class="' . $objType->class . '" data-parent="'.htmlspecialchars($schema).'">'.__(ucfirst($objPural)).'</span>';
-			if(count($objects) > 0) {
-				//print '<span class="count">'.count($objects).'</span>';
-			}
-			$nodes = array();
-			$last_node_id = '';
-			$node_increment = 0;
-			foreach($objects as $key=>$object) {
-				$id = $objType->name[0] . '_' . $schema_id . '_' .Html::id(preg_replace('/ \(.+/', "", $object));
-				if ($id === $last_node_id) {
-					$node_increment += 1;
-				} else {
-					$node_increment = 0;
-				}
-				$last_node_id = $id;
-				$id .= $node_increment;
-				$object = htmlspecialchars($object);
-				$nodes[] = array(
-					"id" => $id,
-					"text" => $object,
-					'icon' => "jstree-file",
-					"a_attr" => array("href" => 'javascript:objDefault(\'' . $objType->name . '\', \''.$id.'\', \''.$schema_id.'\')')
-				);
+    public function __construct($data, $options = null)
+    {
+        $this->data = $data;
+        if ($options and isset($options['objectTypes'])) {
+            $this->objectTypes = $options['objectTypes'];
+        }
+        $this->objectTypes = array_filter($this->objectTypes, array($this, 'typeInUse'));
+        $this->tree = array_map(array($this,'processSchema'), $data['schemas']);
+    }
 
-			}
-			$nodeCount =count($nodes);
-			$groupby = $objType->groupby ? $objType->groupby : $groupby_rule;
-			$grouping = new NodeGrouping($groupby);
-			$nodes = $grouping->getGroupOutput($nodes);
-			//print "</ul></li>\n";
-			$branches[] =  array(
-				'text' => __(ucfirst($objPural)) . ' [' . $nodeCount . ']',
-				'children' => $nodes,
-			);
+    public function getData() {
+        return json_encode($this->tree);
+    }
 
-		}
-	}
-	$trunks[] =  array(
-		'id' => $schema_id,
-		'a_attr' => array('class' => 'schmf'),
-		'state' => array('opened' => 1),
-		'text' => $schema,
-		'children' => $branches,
-	);
+    protected function processSchema($schema) {
+        $schema_id = 's_'.Html::id($schema);
+        $branches = array();
+        foreach ($this->objectTypes as $type) {
+            $branches[] = $this->processObjectType($type, $schema);
+        }
 
-	// print '</ul>';
-	// print '</li>';
+        return array(
+            'id' => $schema_id,
+            'a_attr' => array('class' => 'schmf'),
+            'state' => array('opened' => 1),
+            'text' => $schema,
+            'children' => $branches,
+        );
+    }
+    protected function typeInUse($objectType) {
+        return isset($this->data["{$objectType['name']}s"]);
+    }
+    protected function processObjectType($objectType, $schema) {
+
+        $schema_id = 's_'.Html::id($schema);
+        $objPural = $objectType['name'] . 's';
+        $objects = isset($this->data[$objPural][$schema]) ? $this->data[$objPural][$schema] : array();
+        $nodes = array();
+        $last_node_id = '';
+        $node_increment = 0;
+        $this->messages[] = count($this->data[$objPural][$schema]);
+
+        foreach($objects as $object) {
+
+            $id = $objectType['name'][0] . '_' . $schema_id . '_' .Html::id(preg_replace('/ \(.+/', "", $object));
+            $id = $this->differentiateFunctionIds($objectType['name'], $id, $last_node_id, $node_increment);
+            $object = htmlspecialchars($object);
+            $nodes[] = array(
+                "id" => $id,
+                "text" => $object,
+                'icon' => "jstree-file",
+                "a_attr" => array("href" => 'javascript:objDefault(\'' . $objectType['name'] . '\', \''.$id.'\', \''.$schema_id.'\')')
+            );
+
+        }
+        $nodeCount =count($nodes);
+        $groupby = $objectType['groupby'] ? $objectType['groupby'] : $groupby_rule;
+        $grouping = new NodeGrouping($groupby);
+        $nodes = $grouping->getGroupOutput($nodes);
+        return array(
+            'text' => __(ucfirst($objPural)) . ' [' . $nodeCount . ']',
+            'children' => $nodes,
+        );
+    }
+
+
+    function differentiateFunctionIds($objectType, $id, &$last_node_id, &$node_increment)
+    {
+        if ($objectType !== 'function') {
+            return $id;
+        }
+        if ($id === $last_node_id) {
+            $node_increment += 1;
+        } else {
+            $node_increment = 0;
+        }
+        $last_node_id = $id;
+        return $id . $node_increment;
+
+    }
 }
-print '</div><script>var jsTreeData = ' . json_encode($trunks) . ';</script><div>';
+
+$objTree = new ObjTree($data);
+
+print '</div><script>var jsTreeData = ' . $objTree->getData() . '; </script><div>';
 //print '</ul>';
 ?>
